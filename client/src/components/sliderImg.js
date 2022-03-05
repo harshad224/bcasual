e7c3d62ee3147c55b3d21c93ebd650925f0bc3f8
Harshad Pawar <pawarharshad224@gmail.com>
2022-03-05 18:55:34 +0530

    Latest updates v3

--- client/src/components/sliderImg.js
+++ client/src/components/sliderImg.js
@@ -1,20 +1,20 @@
 export const sliderImg = [
     {
         key: 1,
-        img: "images/shop11.png",
-        title: "SUMMER SALE",
+        img: "images/shirt1.png",
+        title: "CASUAL SHIRTS",
         info: "Don't compromise on style ! Get flat 50% off on new arrivals."
     },
     {
         key: 2,
-        img: "images/nauticaa.png",
-        title: "AUTUMN COLLECTION",
+        img: "images/blackjack1.png",
+        title: "CLASSY JACKETS",
         info: "Don't compromise on style ! Get flat 50% off on new arrivals."
     },
     {
         key: 3,
-        img: "images/shop11.png",
-        title: "LOUNGEWEAR LOVE",
+        img: "images/halftshirrt.png",
+        title: "TRENDY T-SHIRTS",
         info: "Don't compromise on style ! Get flat 50% off on new arrivals."
     },
 
