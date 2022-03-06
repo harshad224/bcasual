e7d63bfffad334842dc20e0dc1363764aef78c1c
Harshad Pawar <pawarharshad224@gmail.com>
2022-03-06 21:59:46 +0530

    Version1.0

--- client/src/App.js
+++ client/src/App.js
@@ -19,8 +19,8 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/products/jackets" element={<ProductList />} />
+        <Route exact path="/products/shirts" element={<ProductList />} />
         <Route exact path="/products/t-shirts" element={<ProductList />} />
-        <Route exact path="/products/pullovers" element={<ProductList />} />
         <Route exact path="/product/:id" element={<Productdetail />} />
         <Route exact path="/cart" element={<Cart />} />
         <Route exact path="/signin" element={userr ? <Navigate to="/" /> : <Login />} />
--- client/src/components/categoryitem.js
+++ client/src/components/categoryitem.js
@@ -1,5 +1,5 @@
 export const categoryitem = [
     { id: 1, image: "images/bomber.png", title: "JACKETS", cat: "jackets" },
-    { id: 2, image: "images/shirt.png", title: "SHIRTS", cat: "t-shirts" },
-    { id: 3, image: "images/tshirts.png", title: "T-SHIRTS", cat: "pullovers" }
+    { id: 2, image: "images/shirt.png", title: "SHIRTS", cat: "shirts" },
+    { id: 3, image: "images/tshirts.png", title: "T-SHIRTS", cat: "t-shirts" }
 ]
